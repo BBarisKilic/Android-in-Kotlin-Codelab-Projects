b94862f1ce948ba6494420496459578893cd325d
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-10-06 17:14:07 +0200

    Created Dice Roller.

---
+++ 02-DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+}
