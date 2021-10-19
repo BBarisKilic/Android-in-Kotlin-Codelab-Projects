2bc21ff0258b122658d83536f6a0e75e041af32d
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-10-19 14:33:51 +0200

    Dice Roller completed.

--- 02-DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ 02-DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +13,32 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
 
-        rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+        rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2;
+            3 -> R.drawable.dice_3;
+            4 -> R.drawable.dice_4;
+            5 -> R.drawable.dice_5;
+            else -> R.drawable.dice_6;
         }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val sides: Int) {
+    fun roll(): Int {
+        return (1..sides).random()
     }
 }
